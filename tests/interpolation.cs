c0ae2b722060e9fb613d3d2b5382b2e8fabb4801
Ovsyannikov Dmitry <dmitroy1234@gmail.com>
2025-11-12 20:37:28 +0300

    Revert "Merge pull request #1 from SergeyGT/OvsyannikovParser"
    This reverts commit 2172e68dfa0b5501283317b949588b03c6dfd296, reversing
    changes made to 031f37fed0f9106b012b998bcf51be00196bf99b.

--- tests/interpolation.cs
+++
@@ -1,5 +0,0 @@
-$"Hello {name}";
-$"Value: {x + y}";
-$"Result: {Calculate()}";
-
-$"Name: {firstName} {lastName}, Age: {age}";
--- tests/test_ligic_op.cs
+++
@@ -1,5 +0,0 @@
-true && false;
-a || b;
-!flag;
-x > y && z < w;
-a == b || c != d;
