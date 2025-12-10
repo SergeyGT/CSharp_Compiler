a98a5b3e96e86a16d548b8ecedf36e794ad90c8f
Burdenyuk Sergey <79414642+SergeyGT@users.noreply.github.com>
2025-12-10 09:27:07 +0300

    Добавлены файлы для реализации дерева

---
+++ Tree/Program.h
@@ -0,0 +1,55 @@
+#pragma once
+#include "Class.h"
+#include "Enum.h"
+#include "Interface.h"
+// ReSharper disable once CppUnusedIncludeDirective
+#include "AccessExpr.h"
+
+struct NamespaceMembersNode final : Node
+{
+    std::vector<ClassDeclNode*> Classes;
+    std::vector<EnumDeclNode*> Enums;
+	std::vector<InterfaceDeclNode*> Interfaces;
+
+    void Add(ClassDeclNode* node) { Classes.push_back(node); }
+    void Add(EnumDeclNode* node) { Enums.push_back(node); }
+	void Add(InterfaceDeclNode* node) {Interfaces.push_back(node);}
+
+    NamespaceMembersNode() noexcept = default;
+
+    [[nodiscard]] std::string_view Name() const noexcept override { return "NamespaceMembers"; }
+};
+
+struct NamespaceDeclNode final : Node
+{
+    std::string_view NamespaceName;
+    NamespaceMembersNode* Members;
+
+
+    NamespaceDeclNode(const std::string_view namespaceName, NamespaceMembersNode* const members)
+        : NamespaceName{ namespaceName }
+      , Members{ members }
+    {
+    }
+
+    [[nodiscard]] std::string_view Name() const noexcept override { return "NamespaceDecl"; }
+};
+
+struct UsingDirectiveNode final : Node
+{
+    IdentifierList* Arg;
+
+    explicit UsingDirectiveNode(IdentifierList* const arg)
+        : Arg{ arg }
+    {
+    }
+
+    [[nodiscard]] std::string_view Name() const noexcept override { return "Using"; }
+};
+
+struct UsingDirectives final : NodeSeq<UsingDirectives, UsingDirectiveNode>
+{
+    using NodeSeq<UsingDirectives, UsingDirectiveNode>::NodeSeq;
+
+    [[nodiscard]] std::string_view Name() const noexcept override { return "UsingDirectives"; }
+};
