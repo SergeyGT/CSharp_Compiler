859120751355e4ce1f45cac88e039db2415105d0
Ivan Shatalov <shatalov17super@gmail.com>
2026-01-09 14:16:10 +0300

    Добавлена правильная реализация недостающих методов

--- Tree/Qualified_or_expr.cpp
+++ Tree/Qualified_or_expr.cpp
@@ -109,93 +109,53 @@ Qualified_or_expr* Qualified_or_expr::FromNull()
     return qoe;
 }
 
-DataType Qualified_or_expr::ToDataType() const {
-    DataType result;
-
-    switch (Type) {
-        case TypeT::Integer:
-            result.AType = DataType::TypeT::Int;
-            break;
-
-        case TypeT::Float:
-            result.AType = DataType::TypeT::Float;
-            break;
-
-        case TypeT::String:
-            result = STD_STRING_TYPE;
-            break;
-
-        case TypeT::Char:
-            result.AType = DataType::TypeT::Char;
-            break;
-
-        case TypeT::Bool:
-            result.AType = DataType::TypeT::Bool;
-            break;
-
-        case TypeT::Identifier:
-            if (Identifier == "null") {
-                result.AType = DataType::TypeT::Null;
-            } else {
-                result.AType = DataType::TypeT::Complex;
-                std::string id(Identifier);
-                size_t start = 0;
-                size_t end = id.find('.');
-                while (end != std::string::npos) {
-                    result.ComplexType.push_back(id.substr(start, end - start));
-                    start = end + 1;
-                    end = id.find('.', start);
-                }
-                result.ComplexType.push_back(id.substr(start));
-            }
-            break;
-
-        case TypeT::ComplexArrayType:
-            if (Previous) {
-                result = Previous->ToDataType();
-                result.ArrayArity++;
-            } else {
-                result.AType = DataType::TypeT::Complex;
-                result.IsUnknown = true;
-            }
-            break;
-
-        case TypeT::ArrayElementExpr:
-            if (Previous) {
-                DataType baseType = Previous->ToDataType();
-                if (baseType.ArrayArity > 0) {
-                    baseType.ArrayArity--;
-                    result = baseType;
-                } else {
-                    result.AType = DataType::TypeT::Complex;
-                    result.IsUnknown = true;
-                }
-            } else {
-                result.AType = DataType::TypeT::Complex;
-                result.IsUnknown = true;
-            }
-            break;
-
-        case TypeT::Dot:
-        case TypeT::SimpleMethodCall:
-        case TypeT::DotMethodCall:
-            result.AType = DataType::TypeT::Complex;
-            result.IsUnknown = true;
-            break;
-
-        case TypeT::Expr:
-            if (Child) {
-                result = Child->AType;
-            } else {
-                result.AType = DataType::TypeT::Void;
-            }
-            break;
-
-        default:
-            result.AType = DataType::TypeT::Complex;
-            result.IsUnknown = true;
-            break;
+DataType Qualified_or_expr::ToDataType() const
+{
+    if (Type == TypeT::ComplexArrayType)
+    {
+        auto childType = Previous->ToDataType();
+        childType.ArrayArity += 1;
+        return childType;
     }
 
-    return result;
+    if (Type == TypeT::Identifier)
+    {
+        DataType data;
+        data.AType = DataType::TypeT::Complex;
+        data.ComplexType = { std::string{ Identifier } };
+        return data;
+    }
+
+    if (Type == TypeT::Dot)
+    {
+        auto childType = Previous->ToDataType();
+        childType.ComplexType.emplace_back(Identifier);
+        return childType;
+    }
+
+    if (Type == TypeT::ArrayElementExpr)
+    {
+        auto childType = Previous->ToDataType();
+        childType.ArrayArity += 1;
+        return childType;
+    }
+
+    return { DataType::TypeT::Void, {}, true };
+}
+
+void Qualified_or_expr::CallForAllChildren(const std::function<void(Qualified_or_expr*)>& function) const
+{
+    if (Previous)
+        function(Previous);
+
+    auto exprFunctor = [&function](const ExprNode* node)
+    {
+        if (!node) { return; }
+        if (node->Child && node->Child->Access) { function(node->Child->Access); }
+        if (node->Child && node->Child->ArrayExpr) { function(node->Child->ArrayExpr); }
+    };
+
+    if (Child) { Child->CallForAllChildren(exprFunctor); }
+
+    if (Arguments) { for (auto* arg : Arguments->GetSeq()) { arg->CallForAllChildren(exprFunctor); } }
 }
