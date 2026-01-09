5fc91796f76c9bb6eaf24291fe5c9a25cb5dca1a
Ivan Shatalov <shatalov17super@gmail.com>
2026-01-09 14:11:45 +0300

    Добавлена реализация метода ToDataType

--- Tree/Qualified_or_expr.cpp
+++ Tree/Qualified_or_expr.cpp
@@ -108,3 +108,94 @@ Qualified_or_expr* Qualified_or_expr::FromNull()
     qoe->Identifier = "null";
     return qoe;
 }
+
+DataType Qualified_or_expr::ToDataType() const {
+    DataType result;
+
+    switch (Type) {
+        case TypeT::Integer:
+            result.AType = DataType::TypeT::Int;
+            break;
+
+        case TypeT::Float:
+            result.AType = DataType::TypeT::Float;
+            break;
+
+        case TypeT::String:
+            result = STD_STRING_TYPE;
+            break;
+
+        case TypeT::Char:
+            result.AType = DataType::TypeT::Char;
+            break;
+
+        case TypeT::Bool:
+            result.AType = DataType::TypeT::Bool;
+            break;
+
+        case TypeT::Identifier:
+            if (Identifier == "null") {
+                result.AType = DataType::TypeT::Null;
+            } else {
+                result.AType = DataType::TypeT::Complex;
+                std::string id(Identifier);
+                size_t start = 0;
+                size_t end = id.find('.');
+                while (end != std::string::npos) {
+                    result.ComplexType.push_back(id.substr(start, end - start));
+                    start = end + 1;
+                    end = id.find('.', start);
+                }
+                result.ComplexType.push_back(id.substr(start));
+            }
+            break;
+
+        case TypeT::ComplexArrayType:
+            if (Previous) {
+                result = Previous->ToDataType();
+                result.ArrayArity++;
+            } else {
+                result.AType = DataType::TypeT::Complex;
+                result.IsUnknown = true;
+            }
+            break;
+
+        case TypeT::ArrayElementExpr:
+            if (Previous) {
+                DataType baseType = Previous->ToDataType();
+                if (baseType.ArrayArity > 0) {
+                    baseType.ArrayArity--;
+                    result = baseType;
+                } else {
+                    result.AType = DataType::TypeT::Complex;
+                    result.IsUnknown = true;
+                }
+            } else {
+                result.AType = DataType::TypeT::Complex;
+                result.IsUnknown = true;
+            }
+            break;
+
+        case TypeT::Dot:
+        case TypeT::SimpleMethodCall:
+        case TypeT::DotMethodCall:
+            result.AType = DataType::TypeT::Complex;
+            result.IsUnknown = true;
+            break;
+
+        case TypeT::Expr:
+            if (Child) {
+                result = Child->AType;
+            } else {
+                result.AType = DataType::TypeT::Void;
+            }
+            break;
+
+        default:
+            result.AType = DataType::TypeT::Complex;
+            result.IsUnknown = true;
+            break;
+    }
+
+    return result;
+}
