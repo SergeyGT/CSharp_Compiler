2adf83dff49ad1ab2a8fab6fe07b99b3a02148b1
Burdenyuk Sergey <79414642+SergeyGT@users.noreply.github.com>
2026-01-09 15:57:25 +0300

    Тест на цикл

---
+++ lexer-build/ForLoop.cs
@@ -0,0 +1,17 @@
+namespace Test {
+    public class Program {
+        public static void Main() {
+
+            Console.Write("[");
+            for (int i = 0; i < array.Length; ++i)
+            {
+                Console.Write(array[i]);
+                bool isLast = i == array.Length - 1;
+                if (!isLast)
+                {
+                    Console.Write(", ");
+                }
+            }
+        }
+    }
+}
