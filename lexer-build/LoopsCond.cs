6f7cddea5f17e8e01343af0e00285a9171660882
Burdenyuk Sergey <79414642+SergeyGT@users.noreply.github.com>
2026-01-09 15:47:18 +0300

    Test Update

--- lexer-build/LoopsCond.cs
+++ lexer-build/LoopsCond.cs
@@ -1,13 +1,11 @@
-	namespace Test {
-		public class Program {
-			public static void Main() {
-				float x = 5;
-			}
-		}
+namespace Test {
+    public class Program {
+        public static void Main() {
+            int x = 5;
+        }
 		
-		public class Program2{
-			public static void Main() {
-				char y = "5";
-			}
+		public int GetX(){
+			return x;
 		}
-	}
+    }
+}
