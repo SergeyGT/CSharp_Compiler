e877fc268de3ecc7e53f7fc36cbaf4f5a99dae67
Burdenyuk Sergey <79414642+SergeyGT@users.noreply.github.com>
2026-01-11 16:18:50 +0300

    Исправлен тест для return

--- lexer-build/AllReturnTypes.cs
+++ lexer-build/AllReturnTypes.cs
@@ -2,37 +2,31 @@ namespace ReturnTest
 {
     public class TestClass
     {
-        // 1. return func() - возврат результата вызова функции
-        public int ReturnFunction()
+		public static void Main()
         {
-            return GetNumber();
-        }
+		}
         
         private int GetNumber()
         {
             return 42;
         }
         
-        // 2. return var; - возврат переменной
         public int ReturnVariable()
         {
             int x = 100;
             return x;
         }
         
-        // 3. return ; - возврат без значения (void метод)
         public void ReturnVoid()
         {
             return;
         }
         
-        // 4. return 2+2; - возврат выражения
         public int ReturnExpression()
         {
             return 2 + 2;
         }
         
-        // 5. return с более сложным выражением
         public int ReturnComplexExpression()
         {
             int a = 5;
@@ -40,7 +34,6 @@ namespace ReturnTest
             return (a * b) + (a - b);
         }
         
-        // 6. return в условном операторе
         public string ReturnConditional(int value)
         {
             if (value > 0)
@@ -51,9 +44,9 @@ namespace ReturnTest
             {
                 return "Non-positive";
             }
+			return " ";
         }
         
-        // 7. return с вызовом метода с параметрами
         public int ReturnMethodWithArgs()
         {
             return Multiply(4, 5);
@@ -64,22 +57,5 @@ namespace ReturnTest
             return x * y;
         }
         
-        // 8. return null
-        public int ReturnNull()
-        {
-            return null;
-        }
-        
-        // 9. return new object()
-        public int ReturnNewObject()
-        {
-            return new A();
-        }
-        
-        // 10. return с цепочкой вызовов
-        public string ReturnChain()
-        {
-            return GetPrefix() + GetSuffix();
-        }
 }
 }
